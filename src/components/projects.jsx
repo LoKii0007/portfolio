f19555e9ec5071aca9ea640d0b26c58c47e4ee89
Lokii0007 <lokeshyadav1098@gmail.com>
2024-06-20 18:35:01 +0530

    fix

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -13,31 +13,34 @@ function Projects() {
 
     pinnedSections.forEach((section, index, sections) => {
 
-      let pinHeight = section.offsetHeight
-      console.log('pin Height', pinHeight)
       let img =  section.querySelector('img')
 
-      gsap.to(section, {
-        scrollTrigger: {
-          trigger: section,
-          start: "top top",
-          end: `+=${pinHeight*(sections.length-index)}` ,
-          pin: true,
-          pinSpacing: false,
-          scrub: 1
-        }
-      })
+      img.onload = ()=>{
+        let pinHeight = section.offsetHeight
+        console.log('pin Height', pinHeight)
 
-      gsap.to(img, {
-        scale: 0.5,
-        scrollTrigger: {
-          trigger: section,
-          start: 'top top',
-          end: `+=${pinHeight}`,
-          scrub: 1,
+        gsap.to(section, {
+          scrollTrigger: {
+            trigger: section,
+            start: "top top",
+            end: `+=${pinHeight*(sections.length-index)}` ,
+            pin: true,
+            pinSpacing: false,
+            scrub: 1
+          }
+        })
+  
+        gsap.to(img, {
+          scale: 0.5,
+          scrollTrigger: {
+            trigger: section,
+            start: 'top top',
+            end: `+=${pinHeight}`,
+            scrub: 1,
+          }
         }
+        )
       }
-      )
 
     })
   }, [])
